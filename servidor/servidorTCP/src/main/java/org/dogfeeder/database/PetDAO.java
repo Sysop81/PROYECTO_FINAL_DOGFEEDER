d7ae6985ef06872a9c9ce866bfade0ca1dfe5315
José Ramón López Guillén <jose.rlg27@gmil.com>
2024-06-04 17:43:53 +0200

    Finalizadas las clases para modelar la mascota asociada a DOGFEEDER y realizar el acceso a datos

--- servidor/servidorTCP/src/main/java/org/dogfeeder/database/PetDAO.java
+++ servidor/servidorTCP/src/main/java/org/dogfeeder/database/PetDAO.java
@@ -1,14 +1,19 @@
 package org.dogfeeder.database;
 
-import org.dogfeeder.database.Conexion;
+import org.dogfeeder.cli.CLInterface;
+import org.dogfeeder.model.Logger4j;
 import org.dogfeeder.model.Pet;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Clase PetDAO
+ * Clase de acceso a datos para manejar las E/S de información desde la DB
+ */
 public class PetDAO {
     private Conexion con;
+    private static Logger4j logger = new Logger4j(PetDAO.class);
 
     /**
      * Constructor de clase
@@ -17,6 +22,12 @@ public class PetDAO {
         this.con = con;
     }
 
+    /**
+     * Método getFirstPet
+     * Método encargado de obtener de la DB la primera mascota seleccionada, puesto que esta versión de DOG-FEEDER no
+     * soporta multimascota ;)
+     * @return Pet Mascota asociada a DOGFEEDER
+     */
     public Pet getFirstPet(){
         String query = "SELECT * FROM pet LIMIT 1";
         Pet pet = null;
@@ -27,43 +38,42 @@ public class PetDAO {
             while(rs.next()){
                 pet = new Pet();
                 pet.setID(rs.getInt(1));
-                System.out.println("ID: " + rs.getInt(1));
                 pet.setName(rs.getString(2));
-                System.out.println("name: " + rs.getString(2));
                 pet.setBreed(rs.getString(3));
-                System.out.println("Breed:  " + rs.getString(3));
                 pet.setBirthday(rs.getString(4));
-                System.out.println("Birthday:  " + rs.getString(4));
                 pet.setWeight(rs.getDouble(5));
-                System.out.println("Weight:  " + rs.getDouble(5));
                 pet.setType(rs.getString(6));
-                System.out.println("type:  " + rs.getString(6));
                 pet.setVaccineNotify(rs.getBoolean(7));
-                System.out.println("vac notify:  " + rs.getBoolean(7));
                 pet.setVaccineDay(rs.getInt(8));
-                System.out.println("vac day:  " + rs.getInt(8));
                 pet.setGetVaccineMonth(rs.getInt(9));
-                System.out.println("vac MONTH:  " + rs.getInt(9));
-
             }
             
             rs.close();
             ps.close();
 
         } catch (SQLException e) {
-            System.out.println("Error al obtener la mascota " + e.getMessage());
+            var msg = "Error al obtener la mascota " + e.getMessage();
+            CLInterface.showAlertDanger(msg);
+            logger.setError(msg);
         }
 
         return pet;
     }
 
 
+    /**
+     * Método postPet
+     * Eate método, se encarga de la inserción / actualización de la mascota asociada a DOG-FEEDER
+     * @param pet Instancia de mascota con los valores a insertar o actualizar
+     * @return int Resultado de la operación
+     */
     public int postPet(Pet pet){
-        //TODO -> Set in TRANSACTION
+        // INSERT
         var targetPet = this.getFirstPet();
         String query = "INSERT INTO pet (name, breed, birthday, weight, type, vac_notify, vac_day, vac_month) " +
                        "VALUES(?,?,?,?,?,?,?,?)";
 
+        // Si la mascota recuperada de la DB existe, se cambia la query por la de actualización
         if(targetPet != null) {
             // UPDATE
             query = "UPDATE pet " +
@@ -87,11 +97,12 @@ public class PetDAO {
             if(targetPet != null)  ps.setInt(9, targetPet.getID());
 
             retorno = ps.executeUpdate();
-
             ps.close();
 
         } catch (SQLException e) {
-            System.out.println("Error al actualizar los datos de la mascota " + e.getMessage());
+            var msg = "Error al actualizar los datos de la mascota " + e.getMessage();
+            CLInterface.showAlertDanger(msg);
+            logger.setError(msg);
         }
 
         return retorno;
--- servidor/servidorTCP/src/main/java/org/dogfeeder/model/Pet.java
+++ servidor/servidorTCP/src/main/java/org/dogfeeder/model/Pet.java
@@ -1,11 +1,12 @@
 package org.dogfeeder.model;
 
-
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-
+/**
+ * Clase para modelar la mascota asociada a DOG-FEEDER
+ */
 public class Pet {
     public enum TypeOfSize{small,medium,big}
     private int ID;
@@ -16,78 +17,131 @@ public class Pet {
     private TypeOfSize type;
     private boolean vaccineNotify;
     private int vaccineDay;
-    private int getVaccineMonth;
+    private int vaccineMonth;
 
+    /**
+     * Constructor
+     */
     public Pet(){}
 
-    public Pet(String[] data){
-        this.name = data[0];
-        this.breed = data[1];
-        this.birthday = data[2];
-        this.weight = Double.parseDouble(data[3]);
-        this.type = this.convertType(data[4]);
-        this.vaccineNotify = Boolean.parseBoolean(data[5]);
-        this.vaccineDay = Integer.parseInt(data[6]);
-        this.getVaccineMonth = Integer.parseInt(data[7]);
-    }
-
+    /**
+     * Getter para la propiedad ID
+     * @return int Identificador de la mascota
+     */
     public int getID() {
         return ID;
     }
 
+    /**
+     * Setter para la propiedad ID
+     * @param ID Identificador para la mascota
+     */
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    /**
+     * Getter para la propiedad name
+     * @return String Cadena que representa el nombre de la mascota
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter para la propiedad name
+     * @param name Cadena que representa el nombre de la mascota
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter para la propiedad breed
+     * @return String Cadena que representa la raza de la mascota
+     */
     public String getBreed() {
         return breed;
     }
 
+    /**
+     * Setter para la propiedad breed
+     * @param breed Cadena que representa la raza de la mascota
+     */
     public void setBreed(String breed) {
         this.breed = breed;
     }
 
+    /**
+     * Getter para la propiedad birthday
+     * @return String cadena con la fecha de nacimiento de la mascota
+     */
     public String getBirthday() {
         return birthday;
     }
 
+    /**
+     * Getter para la propiedad birthday [Date]
+     * @return Date fecha de nacimiento de la mascota
+     */
     public Date getBirthdayToDate(){
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return  Date.valueOf(LocalDate.parse(birthday, fmt));
     }
 
+    /**
+     * Setter para la propiedad birthday
+     * @param birthday Cadena que representa la fecha de nacimiento de la mascota
+     */
     public void setBirthday(String birthday) {
         this.birthday = birthday;
     }
 
+    /**
+     * Getter para la propiedad weight
+     * @return double Valor que determina el peso de la mascota
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Setter para la propiedad weight
+     * @param weight Valor que determina el peso de la mascota
+     */
     public void setWeight(double weight) {
         this.weight = weight;
     }
 
+    /**
+     * Getter para la propiedad type
+     * @return type Tipo de tamaño segun la raza
+     */
     public TypeOfSize getType() {
         return type;
     }
 
+    /**
+     * Setter para la propiedad type
+     * @param type Tipo de tamaño segun la raza
+     */
     public void setType(TypeOfSize type) {
         this.type = type;
     }
 
+    /**
+     * Setter para la propiedad type
+     * @param type Tipo de tamaño segun la raza
+     */
     public void setType(String type){
         this.type = convertType(type);
     }
 
+    /**
+     * Método convertType
+     * @param type Tipo en cadena de caracteres
+     * @return TypeOfSize Valor equivalente convertido a la enumeracion
+     */
     private TypeOfSize convertType(String type){
         switch (type.toLowerCase()){
             case "medium" : return  TypeOfSize.medium;
@@ -97,33 +151,66 @@ public class Pet {
         return TypeOfSize.small;
     }
 
+    /**
+     * Getter para la propiedad vaccineNotify
+     * @return boolean con el resultado de la propiedad
+     */
     public boolean isVaccineNotify() {
         return vaccineNotify;
     }
 
+    /**
+     * Setter para la propiedad vaccineNotify
+     * @param vaccineNotify Boleano que representa la activación / desactivación de las notificaciones
+     */
     public void setVaccineNotify(boolean vaccineNotify) {
         this.vaccineNotify = vaccineNotify;
     }
 
+    /**
+     * Getter para la propiedad vaccineDay
+     * @return int Valor que representa el día de vacunación
+     */
     public int getVaccineDay() {
         return vaccineDay;
     }
 
+    /**
+     * Setter para la propiedad vaccineDay
+     * @param vaccineDay Valor entero que representa el día de vacunación
+     */
     public void setVaccineDay(int vaccineDay) {
         this.vaccineDay = vaccineDay;
     }
 
+    /**
+     * Getter para el mes de vacunación
+     * @return int Valor que represnta el mes de vacunación
+     */
     public int getGetVaccineMonth() {
-        return getVaccineMonth;
+        return vaccineMonth;
     }
 
-    public void setGetVaccineMonth(int getVaccineMonth) {
-        this.getVaccineMonth = getVaccineMonth;
+    /**
+     * Setter para la propiedad vaccineMonth
+     * @param vaccineMonth Valor que represnta el mes de vacunación
+     */
+    public void setGetVaccineMonth(int vaccineMonth) {
+        this.vaccineMonth = vaccineMonth;
     }
 
     @Override
     public String toString() {
-        return  ID + "_" + name + "_" + breed + "_" + birthday + "_" + weight +
-                "_" + type + "_" + vaccineNotify + "_" + vaccineDay + "_" + getVaccineMonth ;
+        return "Pet{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", birthday='" + birthday + '\'' +
+                ", weight=" + weight +
+                ", type=" + type +
+                ", vaccineNotify=" + vaccineNotify +
+                ", vaccineDay=" + vaccineDay +
+                ", vaccineMonth=" + vaccineMonth +
+                '}';
     }
 }
