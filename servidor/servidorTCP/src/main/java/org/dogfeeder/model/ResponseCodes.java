ba7be9cdf227bf09f6492fdd04d7467536e75fa8
José Ramón López Guillén <jose.rlg27@gmil.com>
2024-06-04 17:46:38 +0200

    Generados los enumerados para el manejo de estados y códigos de respuesta del servidor

---
+++ servidor/servidorTCP/src/main/java/org/dogfeeder/model/ResponseCodes.java
@@ -0,0 +1,24 @@
+package org.dogfeeder.model;
+
+/**
+ * Enum ResponseCodes
+ * Códigos de estado para el Servidor
+ */
+public enum ResponseCodes {
+    ERROR(0),        // Códido de respuesta de error genérico
+    OK(1),           // Código de respuesta correcta genérico
+    EMPTY_DATA(2),
+    ERROR_SERVO_IN_USE(3),
+    ERROR_STUCK_HOPPER(4),
+    ERROR_EMPTY_HOPPER(5);
+
+    private final int CODE;
+
+    ResponseCodes(int code){
+        this.CODE = code;
+    }
+
+    public String getCode(){
+        return String.valueOf(CODE);
+    }
+}
---
+++ servidor/servidorTCP/src/main/java/org/dogfeeder/model/ServerStateCodes.java
@@ -0,0 +1,36 @@
+package org.dogfeeder.model;
+
+/**
+ * Enum ServerStateCodes
+ * Códigos de estado para el manejo del servicio
+ */
+public enum ServerStateCodes {
+    CLOSE(-1),
+    LOGIN(0),
+    REGISTER(1),
+    RECOVERYPASS(2),
+    FOOD(3),
+    AUDITS(4),
+    LASTAUDIT(5),
+    GRAPHAUDITS(6),
+    PETDATA(7),
+    POSTPET(8),
+    SETTINGSDATA(9),
+    LED(10),
+    HOPPERLOW(11),
+    FEEDEREMPTY(12),
+    MAXFOODRATION(13),
+    REPORT(14),
+    IDLE_STATE(20);
+
+    private final int STATUS_CODE;
+
+    ServerStateCodes(int code){
+        this.STATUS_CODE = code;
+    }
+
+    public int getStatusCode(){
+        return STATUS_CODE;
+    }
+
+}
