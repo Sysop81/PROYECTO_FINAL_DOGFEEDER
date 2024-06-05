3c352ad20041e17755ca56d532e3f801e02bfb55
José Ramón López Guillén <jose.rlg27@gmil.com>
2024-06-05 19:52:51 +0200

    Añadido javadoc y metodo en clase DAO para obtener los mails válidos para el servicio de notificaciones

--- servidor/servidorTCP/src/main/java/org/dogfeeder/database/UserDAO.java
+++ servidor/servidorTCP/src/main/java/org/dogfeeder/database/UserDAO.java
@@ -1,14 +1,20 @@
 package org.dogfeeder.database;
 
-import org.dogfeeder.database.Conexion;
+import org.dogfeeder.cli.CLInterface;
+import org.dogfeeder.model.Logger4j;
 import org.dogfeeder.model.User;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+/**
+ * Clase UserDAO
+ * Clase de acceso a datos para manejar las E/S de información desde la DB
+ */
 public class UserDAO {
     private Conexion con;
+    private static Logger4j logger = new Logger4j(UserDAO.class);
 
     /**
      * Constructor de clase
@@ -17,6 +23,12 @@ public class UserDAO {
         this.con = con;
     }
 
+    /**
+     * Método postUser
+     * Método encargado de dar persistencia a un nuevo ususario en la DB
+     * @param user Instancia de usuario que se pretende almacenar en la DB
+     * @return int Resultado de la operación realizada
+     */
     public int postUser(User user){
         String query = "INSERT INTO user (email,password) VALUES(?,?)";
         int retorno = 0;
@@ -30,12 +42,20 @@ public class UserDAO {
             ps.close();
 
         } catch (SQLException e) {
-            System.out.println("Error al consultar la existencia de la base de datos " + e.getMessage());
+            var msg = "Error al insertar el usuario con email " + user.getEmail() + "." + e.getMessage();
+            CLInterface.showAlertDanger(msg);
+            logger.setError(msg);
         }
 
         return retorno;
     }
 
+    /**
+     * Método getUserbyEmail
+     * Método encargado de recuperar al usuario mediante su dirección de correo electrónico
+     * @param email Email del usuario
+     * @return User Instancia de usuario al que pertenece el correo
+     */
     public User getUserbyEmail(String email){
         String query = "SELECT * FROM user WHERE email = ?";
         User user = null;
@@ -47,25 +67,31 @@ public class UserDAO {
             while(rs.next()){
                 user = new User();
                 user.setID(rs.getInt(1));
-                System.out.println("ID: " + rs.getInt(1));
                 user.setEmail(rs.getString(2));
-                System.out.println("email: " + rs.getString(2));
                 user.setPassword(rs.getString(3));
-                System.out.println("Pass:  " + rs.getString(3));
+                user.setValidMail(rs.getBoolean(4));
             }
-
 
             rs.close();
             ps.close();
 
         } catch (SQLException e) {
-            System.out.println("Error al consultar la existencia de la base de datos " + e.getMessage());
+            var msg = "Error al obtener el usuario mediante su dirección de email " + e.getMessage();
+            CLInterface.showAlertDanger(msg);
+            logger.setError(msg);
+
         }
 
         return user;
     }
 
-
+    /**
+     * Método setNewPassword
+     * Método encargado de establecer una nueva contraseña al usuario.
+     * @param email Correo del usuario del cual se pretende cambiar su contraseña
+     * @param password Cadena hasheada que representa la nueva contraseña de usuario
+     * @return int Resultado de la operación
+     */
     public int setNewPassword(String email, String password){
         String query = "UPDATE user SET password = ? WHERE email = ?";
         int retorno = 0;
@@ -79,22 +105,44 @@ public class UserDAO {
             ps.close();
 
         } catch (SQLException e) {
-            System.out.println("Error al actualizar el password de usuario " + e.getMessage());
+            var msg = "Error al actualizar el password de usuario " + e.getMessage();
+            CLInterface.showAlertDanger(msg);
+            logger.setError(msg);
         }
 
         return retorno;
     }
 
-    /**
-     * Metodo para cerrar la conexion con la BBDD
-     * @return true o false.
-     */
-    /*public void close(){
-        try {
-            this.con.cerrarConexion();
-        } catch (SQLException e) {
-            System.out.println("Error al cerrar la conexión UsuarioDAO " + e.getMessage());
-        }
-    }*/
 
+    /**
+     * Método getAllValidMail
+     * Método encargado de obtener los correos electrónicos de aquellos usuarios marcados como mail válido
+     * Método encargado de llevar al cliente los 100 últimos registros de auditoría
+     * @return ArrayList<String> Listado con los correos de los usuarios a los que se notificará
+     */
+    public ArrayList<String> getAllValidMail(){
+        String query = "SELECT email " +
+                       "FROM user " +
+                       "WHERE valid_mail = TRUE " +
+                       "ORDER BY ID";
+        ArrayList<String> listOfMails = new ArrayList<>();
+
+        try {
+            PreparedStatement ps = con.getConexion().prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                listOfMails.add(rs.getString(1));
+            }
+
+            rs.close();
+            ps.close();
+
+        } catch (SQLException e) {
+            var msg = "Error al obtener los correo electrónicos de los usuarios " + e.getMessage();
+            CLInterface.showAlertDanger(msg);
+            logger.setError(msg);
+        }
+
+        return listOfMails;
+    }
 }
--- servidor/servidorTCP/src/main/java/org/dogfeeder/model/User.java
+++ servidor/servidorTCP/src/main/java/org/dogfeeder/model/User.java
@@ -1,39 +1,104 @@
 package org.dogfeeder.model;
 
+/**
+ * Clase User
+ * Modela instancias de tipo usuario del servicio DOG-FEEDER
+ */
 public class User {
     private int ID;
     private String email;
     private String password;
+    private boolean isValidMail;
 
+    /**
+     * Constructor
+     */
     public User(){}
 
+    /**
+     * Constructor con parametros
+     * @param email Correo electrónico del usuario
+     * @param password Contraseña del usuario
+     */
     public User(String email, String password){
         this.email = email;
         this.password = password;
     }
 
+    /**
+     * Constructor con parametros [Sobrecarga]
+     * @param email Correo electronico del usuario
+     * @param password Contraseña del usuario
+     * @param isValidMail Booleano que determina si el correo es valido para notificaciones
+     */
+    public User(String email, String password,boolean isValidMail){
+        this.email = email;
+        this.password = password;
+        this.isValidMail = isValidMail;
+    }
+
+    /**
+     * Getter para la porpiedad ID
+     * @return int Representa el identificacor del usuario
+     */
     public int getID() {
         return ID;
     }
 
+    /**
+     * Setter para la propiedad ID
+     * @param ID int Valor que determina el identificador de usuario
+     */
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    /**
+     * Getter pora la propiedad email
+     * @return String Correo electrónico del usuario
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter para la propiedad email
+     * @param email Correo electrónico del usuario
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Getter para la propiedad password
+     * @return String Contraseña del usuario
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter para la propiedad password
+     * @param password Contraseña del usuario
+     */
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    /**
+     * Getter para la propiedad validEmail
+     * @return boolean Determina si es un correo válido para notificaciones o no
+     */
+    public boolean isValidMail() {
+        return isValidMail;
+    }
+
+    /**
+     * Setter para la propiedad validMail
+     * @param validMail Determina si el correo es valido para las notificaciones
+     */
+    public void setValidMail(boolean validMail) {
+        isValidMail = validMail;
     }
 
     @Override
@@ -42,6 +107,7 @@ public class User {
                 "ID=" + ID +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
+                ", isValidMail=" + isValidMail +
                 '}';
     }
 }
