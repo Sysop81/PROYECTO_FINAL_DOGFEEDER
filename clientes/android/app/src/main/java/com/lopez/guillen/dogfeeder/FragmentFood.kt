1aef4b4c03e66c75ef9bd1bcf28af8fe6bf7de01
José Ramón López Guillén <jose.rlg27@gmil.com>
2024-06-07 19:01:20 +0200

    Finalizado el activity MAIN y el fragment para el suministro de alimento

--- clientes/android/app/src/main/java/com/lopez/guillen/dogfeeder/FragmentFood.kt
+++ clientes/android/app/src/main/java/com/lopez/guillen/dogfeeder/FragmentFood.kt
@@ -1,29 +1,44 @@
 package com.lopez.guillen.dogfeeder
 
 import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.lopez.guillen.dogfeeder.model.AuditFoodItem
+import com.lopez.guillen.dogfeeder.model.Pet
 import com.lopez.guillen.dogfeeder.model.Session
 import com.lopez.guillen.dogfeeder.utils.Tools
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import java.io.DataInputStream
-import java.io.DataOutputStream
-import java.io.InputStream
-import java.io.OutputStream
+import org.json.JSONObject
+import java.net.SocketException
 
-class FragmentFood(val _context: Activity) : Fragment() {
-
+/**
+ * Clase FragmentFood
+ * Clase encargada de la lógica de negocio para la vista de suministro de alimento a la mascota asociada a DOG-FEEDER
+ */
+class FragmentFood(val _context: Activity) : Fragment(){
+    // Propiedades de clase
     private val session = Session.getInstance()
     private lateinit var btnSetFood: Button
+    private var pet : Pet? = null
+    private lateinit var auditFood : AuditFoodItem
+    private lateinit var wvLastSupplyFood : WebView
+    private lateinit var wvHooperState : WebView
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,37 +48,195 @@ class FragmentFood(val _context: Activity) : Fragment() {
         return inflater.inflate(R.layout.fragment_food, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btnSetFood = view.findViewById<Button>(R.id.btnSetFood)
-        btnSetFood.setOnClickListener{
-            handleBtnSetFood()
+        // Step 0. Manejamos la visibilidad de la searchView y comprobamos si en este fragment es necesaria la acción de
+        //         refresco.
+        (activity as MainActivity).manageSearchView(false)
+        Tools.activateMainSwipeRefresh(_context,true)
+
+        // Step 1. Instanciado de controles de la vista y demas objetos
+        sharedPreferences = view.context.getSharedPreferences("preferences", Context.MODE_PRIVATE)
+        wvLastSupplyFood = view.findViewById(R.id.webViewLastSupplyFood)
+        wvHooperState = view.findViewById(R.id.webViewHooperState)
+        btnSetFood = view.findViewById(R.id.btnSetFood)
+        progressBar = view.findViewById(R.id.progressBar)
+        progressBar.visibility = View.VISIBLE
+
+        // Step 2. Definición de manejadores de eventos
+        btnSetFood.setOnClickListener{ handleBtnSetFood() }
+
+        // Step 3. Obtenemos de la sesión los datos o consultamos al servidor
+        session.getSessionPet()?.let { pet ->
+            this.pet = pet
+            setDataInForm()
+        } ?: getPetData()
+    }
+
+
+    /**
+     * Método getPetData
+     * Este método se encarga de recuperar los datos almacenados de la mascota asociada al comedero
+     */
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getPetData(){
+        var response = Session.ServerResponseCodes.ERROR.code
+        val handler = Handler(Looper.getMainLooper())
+        var msg = _context.getString(R.string.info_error_connection)
+        var isConError = false
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                // Step 1.  Realizamos el envío de datos al servidor
+                session.sendRequestToServer(null, Session.ServerStates.PETDATA)
+
+                // Step 2. Obtenemos la respuesta del servidor
+                response = session.readResponseFromServer()
+            } catch (se: SocketException) {
+                session.clientSocket.close()
+                isConError = true
+            } finally {
+                handler.post{
+                    if(!isConError){
+                        if(!response.equals(Session.ServerResponseCodes.ERROR.code)){
+                            // Establecemos la mascota en la sesion
+                            val job = JSONObject(response)
+                            session.setSessionPet(Pet(
+                                job.getInt("ID"),
+                                job.getString("name"),
+                                job.getString("breed"),
+                                job.getString("birthday"),
+                                job.getDouble("weight"),
+                                job.getString("type"),
+                                job.getBoolean("vaccineNotify"),
+                                job.getInt("vaccineDay") -1,
+                                job.getInt("vaccineMonth")
+                            ))
+                            // Seteamos los valores en el formulario y realizamos un retorno para completar la accón
+                            setDataInForm()
+                            return@post
+                        }else{
+                            msg = getString(R.string.info_error_not_register_pet)
+                        }
+                    }
+                    progressBar.visibility = View.GONE
+                    Tools.showAlertDialog(_context,msg,R.drawable.ic_baseline_error)
+                }
+            }
         }
     }
 
+
+    /**
+     * Método setDataInForm
+     * Este método se encarga de setear los valores obtenidos para la mascota y última toma de alimento registrado en la
+     * vista.
+     */
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setDataInForm(){
+        // Step 1. Instanciamos la mascota desde el singletone
+        pet = session.getSessionPet()
+        // Step 2. Recuperamos el último registro de comida suministrado y el estado de la tolva
+        getLastSupplyFoodAudit()
+    }
+
+
+    /**
+     * Método getLastSupplyFoodAudit
+     * Este método se encarga de recuperar el último registro de alimento suministrado y el estado de la tolva, con la
+     * finalidad de cargar los controles de tipo webview
+     */
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getLastSupplyFoodAudit(){
+        var response = Session.ServerResponseCodes.ERROR.code
+        val handler = Handler(Looper.getMainLooper())
+        var msg = _context.getString(R.string.info_error_connection)
+        var isConError = false
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                // Step 1.  Realizamos el envío de datos al servidor
+                session.sendRequestToServer(null, Session.ServerStates.LASTAUDIT)
+
+                // Step 2. Obtenemos la respuesta del servidor
+                response = session.readResponseFromServer()
+            } catch (se: SocketException) {
+                session.clientSocket.close()
+                isConError = true
+            } finally {
+                handler.post{
+                    progressBar.visibility = View.GONE
+                    if(!isConError){
+                        if(!response.equals(Session.ServerResponseCodes.ERROR.code)){
+                            // Obtenemos los datos de la respuesta del servidor
+                            val job = JSONObject(response)
+                            // Instanciamos un registro de auditoría y el estado de la tolva
+                            var hopperStatus = job.getString("hopperStatus")
+                            auditFood = AuditFoodItem(
+                                job.getInt("ID"),
+                                job.getString("user"),
+                                Tools.formatDate(job.getString("date"),"-"),
+                                job.getString("time"),
+                                job.getDouble("weight")
+                            )
+
+                            // Realizamos la carga de los controles webView
+                            wvLastSupplyFood.loadData(Tools.generateWebViewLastSupplyFoodContent(auditFood, getString(R.string.txt_last_food_supplied, pet?.name)),
+                                "text/html", "UTF-8")
+                            wvHooperState.loadData(Tools.generateWebViewHopperState(hopperStatus,getString(R.string.txt_hopper_state)),
+                                "text/html", "UTF-8")
+                            return@post
+                        }else{
+                            msg = getString(R.string.info_error_not_register_pet)
+                        }
+                    }
+                    Tools.showAlertDialog(_context,msg,R.drawable.ic_baseline_error)
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Manejador de eventos handleBtnSetFood
+     * Este manejador se encarga de gestionar el suministro de alimento a la mascota haciendo girar el servoMotor de
+     * 360 grados encargado de arrastrar el alimento desde la tolva hasta el cuenco contenedor de alimento.
+     */
     private fun handleBtnSetFood(){
         btnSetFood.isEnabled = false
+        var statusCode = Session.ServerResponseCodes.ERROR.code
         val handler = Handler(Looper.getMainLooper())
-        val that = this
-        GlobalScope.launch(Dispatchers.IO) {
-            val stream: OutputStream =  that.session.clientSocket.getOutputStream()
-            val streamOut = DataOutputStream(stream)
-            streamOut.writeUTF("FOOD")
+        var msg = _context.getString(R.string.info_error_connection)
+        var isConError = false
+        var icon = R.drawable.ic_baseline_error
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                // Step 1.  Realizamos el envío de datos al servidor
+                session.sendRequestToServer(null, Session.ServerStates.FOOD)
 
-            val inStream: InputStream = that.session.clientSocket.getInputStream()
-            val data = DataInputStream(inStream)
-            val statusCode = data.readUTF();
+                // Step 2. Obtenemos la respuesta del servidor
+                statusCode = session.readResponseFromServer()
+            } catch (se: SocketException) {
+                session.clientSocket.close()
+                isConError = true
+            } finally {
+                handler.post {
+                    if(!isConError){
+                        msg = when(statusCode){
+                            Session.ServerResponseCodes.ERROR.code -> getString(R.string.info_error_food_supplied)
+                            Session.ServerResponseCodes.OK.code ->  getString(R.string.info_ok_food_supplied)
+                            Session.ServerResponseCodes.WAIT.code-> getString(R.string.info_wait_food_supplied)
+                            Session.ServerResponseCodes.HOPPER_DANGER.code -> getString(R.string.info_error_empty_hopper)
 
-            handler.post{
-
-                val msg = when(statusCode){
-                    "1" -> "Comida suministrada"
-                    "2" -> "Comida no suministrada"
-                    else -> "Espere!!! El servomotor para el suministro de alimentos está en uso"
+                            else -> getString(R.string.info_error_stuck_hopper)
+                        }
+                        // Cambiamos el icono a info
+                        if(statusCode.equals(Session.ServerResponseCodes.OK.code)) icon = R.drawable.ic_baseline_info
+                    }
+                    // Informamos al usuario
+                    Tools.showAlertDialog(_context,msg,icon)
+                    btnSetFood.isEnabled = true
                 }
-                Tools.showAlertDialog(_context,msg)
-                btnSetFood.isEnabled = true
             }
         }
     }
--- clientes/android/app/src/main/java/com/lopez/guillen/dogfeeder/MainActivity.kt
+++ clientes/android/app/src/main/java/com/lopez/guillen/dogfeeder/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Handler
 import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
@@ -17,31 +18,38 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.SearchView
 import androidx.core.animation.doOnEnd
 import androidx.core.view.GravityCompat
+import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import com.lopez.guillen.dogfeeder.model.Session
 import com.lopez.guillen.dogfeeder.utils.Tools
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import java.io.DataInputStream
-import java.io.DataOutputStream
-import java.io.InputStream
-import java.io.OutputStream
 
+/**
+ * Clase MainActivity
+ * Activity MAIN o principal, es la actividad que se carga despues de que un usuario realice la acción de LOGIN en la
+ * app. Sobre esta activity se establecen los cuatro fragments de gestión para DOG-FEEDER
+ */
 class MainActivity : AppCompatActivity() {
-
+    // Propiedades de clase
+    private lateinit var swipeRefresh : SwipeRefreshLayout
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var searchItem:  MenuItem
+    private lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Step 0. Obtenemos el contexto del activity y lanzamos una corrutina para establecer la progressbar
         val that = this
-        GlobalScope.launch(Dispatchers.Main) {
+        lifecycleScope.launch(Dispatchers.Main) {
             // Establecemos el layout que contine la progressbar
             setContentView(R.layout.progressbar)
 
@@ -58,7 +66,8 @@ class MainActivity : AppCompatActivity() {
             animation.start()
 
             animation.doOnEnd {
-                // Se establece el layout para nuestro mainACTIVITY
+
+                // Se establece el layout para nuestro mainActivity
                 setContentView(R.layout.activity_main)
 
                 // Step 1. Establecemos la Toolbar en lugar de la ActionBar y configuramos el DrawerMenu
@@ -74,6 +83,7 @@ class MainActivity : AppCompatActivity() {
                     R.string.navigation_drawer_close
                 )
                 // Step 3. Se añade un listener para manejar el evento abierto/cerrado y se sincroniza.
+                toggle.drawerArrowDrawable.color = getColor(R.color.white)
                 drawerLayout.addDrawerListener(toggle)
                 toggle.syncState()
 
@@ -90,89 +100,153 @@ class MainActivity : AppCompatActivity() {
                 bottonNavigation.setOnItemSelectedListener(that::manageOptionMenu)
 
                 // Step 7. Declaramos la funcion de callback y establecemos el callback onBackPressed.
-                val callback = object : OnBackPressedCallback(true /* enabled by default */) {
+                val callback = object : OnBackPressedCallback(true) {
                     override fun handleOnBackPressed()
                     {
-                        // Maneja el evento de ir hacia atrás aquí
-                        if (drawerLayout.isDrawerOpen(GravityCompat.START))
-                        {
+                        // Unicamente se desea cerrar el drawerMenu. Se deshabilita cualquier acción sobre los fragments
+                        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
                             drawerLayout.closeDrawer(GravityCompat.START)
                         }
-                        else
-                        {
-                            isEnabled = false
-                            finish()
-                        }
+
                     }
                 }
                 onBackPressedDispatcher.addCallback(that,callback)
 
                 //Step 8. Obtenemos las sharedPreferences
                 sharedPreferences = that.getSharedPreferences("preferences", Context.MODE_PRIVATE)
+
                 // Step 9. Recuperamos los elementos del header del drawerMenu y seteamos al usuario que tenemos en nuestras
                 //         sharedPreferences.
                 val headerView = drawerMenu.getHeaderView(0)
-                //val tvUsuario = headerView.findViewById<TextView>(R.id.tvUserName)
                 val tvEmail = headerView.findViewById<TextView>(R.id.tvEmail)
                 val user_ = Tools.getUserInSharedPreferences(sharedPreferences)
                 tvEmail.setText(user_?.email)
+
+                // Step 10. Instanciamos el objeto swipe y gestionamos un listener para manejar el evento de refresco.
+                swipeRefresh = findViewById(R.id.swipeMain)
+                swipeRefresh.setOnRefreshListener {
+                    val session = Session.getInstance()
+                    val handler = Handler(Looper.getMainLooper())
+                    lifecycleScope.launch(Dispatchers.IO) {
+                        var isConnected = false
+                        try {
+                            isConnected = session.checkServerConnection(that)
+                        }catch(e : Exception){
+
+                        }finally {
+                            handler.post{
+                                if(isConnected){
+                                    if(Tools.checkSharedPreferences(sharedPreferences)){
+                                        val user = Tools.getUserInSharedPreferences(sharedPreferences)
+                                        if(user != null)
+                                            lifecycleScope.launch(Dispatchers.IO){
+                                                user.login(that,true,false)
+                                            }
+                                    }
+                                }else{
+                                    Tools.showAlertDialog(that,getString(R.string.info_error_connection),R.drawable.ic_baseline_error)
+                                }
+
+                                swipeRefresh.isRefreshing = false
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
+
     /**
      * Método onCreateOptionsMenu
      * Se encarga de inflar la vista de la Toolbar cargando el xml del menu en la vista principal
+     * @param menu
+     * @return Boolean
      */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         // Step 1. Inflamos la vista de la Toolbar
         menuInflater.inflate(R.menu.top_app_bar, menu)
-        // Step 2. Inicializamos nuestra searchView que se encuentra contenida en el menu
-        //searchItem = menu?.findItem(R.id.action_search)
-        //searchView = searchItem?.actionView as SearchView
 
-        // Step 3. Inicializamos la ordenacion tambien contenida en el menu
-        //orderView = menu?.findItem(R.id.action_order)
+        // Step 2. Inicializamos nuestra searchView que se encuentra contenida en el menu
+        searchItem = menu?.findItem(R.id.action_search)!!
+        searchView = searchItem.actionView as SearchView
+
+        // Step 3. Establecemos el color para el texto de la searchView
+        val searchText = searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+        searchText.setTextColor(getColor(R.color.white))
+        searchText.setHintTextColor(getColor(R.color.white))
+
+        // Step 4. Ocultamos la searchView para que no se visualice al cargar el primer fragment
+        manageSearchView(false)
 
         return true
     }
+
+
+    /**
+     * [Getter] Método getSearchItem
+     * Getter para la propiedad searchItem.
+     * @return MenuItem
+     */
+    fun getSearchItem(): MenuItem {
+        return searchItem
+    }
+
+
+    /**
+     * [Getter] Método getSearchView
+     * Getter para la propiedad searchView.
+     * @return SearchView
+     */
+    fun getSearchView(): SearchView{
+        return searchView
+    }
+
+
+    /**
+     * [Setter] Método manageSearchView
+     * @param isVisible boolean que representa el estado visible / invisible para la searchView
+     */
+    fun manageSearchView(isVisible : Boolean){
+        searchView.isVisible = isVisible
+        searchItem.isVisible = isVisible
+    }
+
 
     /**
      * Método onOptionsItemSelected
      * Metodo encargado del manejo de eventos sobre el menu de opciones de la Toolbar. En este caso, solo maneja la opción
      * de cierre de sesión y vuelta a la pantalla de login.
+     * @param item Opción de menú seleccionada
+     * @return Boolean
      */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.opCloseSession->{
                 // ***** Cerramos sesion
-                // Step 1 Limpiamos el usuario de las sharedPreferences
-                Tools.cleanSharedPreferences(sharedPreferences)
 
-                /*val user = FirebaseAuth.getInstance().currentUser
-                if(user != null) FirebaseAuth.getInstance().signOut()*/
-
+                // Step 1. Se obtiene una instancia del singletone y se define el manejador para el post de la corrutina
                 val session = Session.getInstance()
-                //session.close()
-
                 val handler = Handler(Looper.getMainLooper())
+                var statusCode = Session.ServerResponseCodes.ERROR.code
+                lifecycleScope.launch(Dispatchers.IO) {
 
-                GlobalScope.launch(Dispatchers.IO) {
-                    val stream: OutputStream =  session.clientSocket.getOutputStream()
-                    val streamOut = DataOutputStream(stream)
-                    streamOut.writeUTF("CLOSE")
+                    // Step 2. Realizamos el envío de datos al servidor
+                    session.sendRequestToServer(null, Session.ServerStates.CLOSE)
 
-                    val inStream: InputStream = session.clientSocket.getInputStream()
-                    val data = DataInputStream(inStream)
-                    val statusCode = data.readUTF();
+                    // Step 3. Obtenemos la respuesta del servidor
+                    statusCode = session.readResponseFromServer()
 
+                    // Step 4. Manejamos la información obtenida del servidor y operamos en consecuencia, mostrando un
+                    //         cuadro de diálogo en caso de error o bien limpiando la sesión y volviendo a la vista de
+                    //         LOGIN
                     handler.post{
-                        if(statusCode != null && statusCode.equals("1")){
+                        if(statusCode.equals(Session.ServerResponseCodes.OK.code)){
                             session.close()
                             Tools.cleanSharedPreferences(sharedPreferences)
                             startActivity(Intent(this@MainActivity, LoginActivity::class.java))
                         }else{
-                            Tools.showAlertDialog(this@MainActivity,"Error al cerrar sesión")
+                            Tools.showAlertDialog(this@MainActivity,getString(R.string.info_error_logout),R.drawable.ic_baseline_error)
                         }
                     }
                 }
@@ -182,33 +256,37 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     /**
      * Método manageOptionMenu. [DrawerMenu && BottonMenu handler options]
      * Se encarga de manejar tanto el drawerMenu como el bottonMenu, llamanado al medoto loadFragment en funcion del
      * elemeto del menu sobre el que se ha recibido el vento. Obviamente tanto para el bottonmenu como para el drwar,
      * se establece el mismo layout
+     * @param item Opción de menú seleccionada para su carga
+     * @return Boolean
      */
     fun manageOptionMenu(item: MenuItem): Boolean{
         return when (item.itemId) {
-            R.id.menuHome -> {
+            R.id.menuFood -> {
                 loadFragment(FragmentFood(this))
                 true
             }
             R.id.menuList -> {
-                //loadFragment(FragmentRv(this))
+                loadFragment(FragmentTab(this))
                 true
             }
-            R.id.menuFav -> {
-                //loadFragment(FragmentRvFav(this))
+            R.id.menuPet -> {
+                loadFragment(FragmentPet(this))
                 true
             }
-            R.id.menuLoc -> {
-                //loadFragment(FragmentLoc(this))
+            R.id.menuSettings -> {
+                loadFragment(FragmentSettings(this))
                 true
             }
             else -> false
         }
     }
+
 
     /**
      * Método loadFragment
